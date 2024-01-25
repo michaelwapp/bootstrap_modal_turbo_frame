9f817f2631ae5eff2b6d57d1994dff053a8a7372
Michael Wapp <wappmichael@gmail.com>
2024-01-25 10:22:33 +0700

    feat: Added Error Handling in Modal Support

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,27 +23,19 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      redirect_to project_url(@project), notice: "Project was successfully created."
+    else
+      render :form_update, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /projects/1 or /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to project_url(@project), notice: "Project was successfully updated." }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update(project_params)
+      redirect_to project_url(@project), notice: "Project was successfully updated."
+    else
+      render :form_update, status: :unprocessable_entity
     end
   end
 
